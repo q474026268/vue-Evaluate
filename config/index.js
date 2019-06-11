aaf617c6e5fef077eef6b0fe1dfdf719f3caf4d9
q474026268 <474026268@qq.com>
2019-06-11 09:35:32 +0800

    no message

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {    //将www.exaple.com印射为/apis
-        target: 'http://172.17.219.39:8090',  // 接口域名
+        target: 'http://127.0.0.1:8090',  // 接口域名
         // secure: false,  // 如果是https接口，需要配置这个参数
         changeOrigin: true,  //是否跨域
         pathRewrite: {
